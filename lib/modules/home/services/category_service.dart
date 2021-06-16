d04ccbbb88504508003ec66ab08b58a8bfcfd1a0
mahmoudsalah37 <mahmoudsalah37@gmail.com>
2021-06-16 14:45:07 +0200

    drop down button

--- lib/modules/home/services/category_service.dart
+++ lib/modules/home/services/category_service.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:toast_app/errors_handler/failure.dart';
+import 'package:toast_app/modules/home/models/category_model.dart';
+import 'package:toast_app/modules/home/models/product_model.dart';
 
 import '../models/categories_model.dart';
 import '../../../services/default_service.dart';
@@ -13,8 +15,9 @@ class CategoryService extends DefaultService {
     try {
       final response = await getDataById(path, id);
       final json = response.data;
-      final categories = CategoriesModel.fromJson(json);
-      return categories;
+      final data = CategoriesModel.fromJson(json);
+      insertAllFilter(data);
+      return data;
     } on SocketException {
       throw Failure('No Internet connection');
     } on HttpException {
@@ -24,5 +27,18 @@ class CategoryService extends DefaultService {
     } catch (e) {
       throw Failure('Exeption');
     }
+  }
+
+  void insertAllFilter(CategoriesModel data) {
+    data.categories?.insert(
+        0, CategoryModel(id: 0, name: 'All', products: fetchAll(data)));
+  }
+
+  List<ProductModel> fetchAll(CategoriesModel categoriesModel) {
+    List<ProductModel> products = [];
+    for (final v in categoriesModel.categories!) {
+      products.addAll(v.products ?? []);
+    }
+    return products;
   }
 }
