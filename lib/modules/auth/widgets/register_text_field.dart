5ac49a553733984a7f0959af51e28a13b997a453
mahmoudsalah37 <mahmoudsalah37@gmail.com>
2021-06-23 15:28:33 +0200

    fix import

---
+++ lib/modules/auth/widgets/register_text_field.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../src/styles.dart';
+
+// ignore: must_be_immutable
+class RegisterTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final TextInputType inputType;
+  final String hint;
+  bool obscureText;
+  final Widget? suffixIcon;
+
+  RegisterTextField({
+    required this.controller,
+    required this.inputType,
+    required this.hint,
+    this.obscureText = false,
+    this.suffixIcon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 10),
+      child: TextField(
+        obscureText: obscureText,
+        controller: controller,
+        style: theme.textTheme.headline2,
+        keyboardType: inputType,
+        textInputAction: TextInputAction.next,
+        decoration: CustomStyle.registerInputDecoration.copyWith(
+          hintText: hint,
+          suffixIcon: suffixIcon,
+        ),
+      ),
+    );
+  }
+}
